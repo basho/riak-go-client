f3c4b0bfbbff7406cfd36d5c1ef1cba0e1ecac8c
Luke Bakken <lbakken@basho.com>
2015-06-29 14:52:57 -0700

    Implement defaultNodeManager

--- node_manager.go
+++ node_manager.go
@@ -1,12 +1,43 @@
 package riak
 
 type NodeManager interface {
-	ExecuteOnNode(nodes []*Node, command Command) (executing bool, err error)
+	ExecuteOnNode(nodes []*Node, command Command, previous *Node) (executed bool, err error)
 }
 
 type defaultNodeManager struct {
+	nodeIndex uint16
 }
 
-func (nm *defaultNodeManager) ExecuteOnNode(nodes []*Node, command Command) (executing bool, err error) {
-	return true, nil
+func (nm *defaultNodeManager) ExecuteOnNode(nodes []*Node, command Command, previous *Node) (executed bool, err error) {
+	executed = false
+	startingIndex := nm.nodeIndex
+
+	for {
+		node := nodes[nm.nodeIndex]
+
+		nm.nodeIndex++
+
+		if int(nm.nodeIndex) == len(nodes) {
+			nm.nodeIndex = 0
+		}
+
+		// don't try the same node twice in a row if we have multiple nodes
+		if len(nodes) > 1 && previous != nil && previous == node {
+			continue
+		}
+
+		if executed, err = node.Execute(command); err != nil {
+			executed = false
+			logErr(err)
+		} else {
+			executed = true
+			break
+		}
+
+		if nm.nodeIndex == startingIndex {
+			break
+		}
+	}
+
+	return
 }
