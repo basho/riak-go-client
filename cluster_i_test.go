9db5b4de2ddf839b8b148ea7b8372f8b9b75426d
Luke Bakken <lbakken@basho.com>
2015-07-01 12:09:06 -0700

    Test concurrent execution of PingCommand

--- cluster_i_test.go
+++ cluster_i_test.go
@@ -60,3 +60,65 @@ func TestExecuteCommandOnCluster(t *testing.T) {
 		t.Errorf("expected %v, got %v", expected, actual)
 	}
 }
+
+func TestExecuteConcurrentCommandsOnCluster(t *testing.T) {
+	nodeOpts := &NodeOptions{
+		MinConnections: 32,
+		MaxConnections: 64,
+		RemoteAddress: remoteAddress,
+	}
+
+	var node *Node
+	var err error
+	if node, err = NewNode(nodeOpts); err != nil {
+		t.Error(err.Error())
+	}
+	if node == nil {
+		t.FailNow()
+	}
+
+	nodes := []*Node{node}
+	opts := &ClusterOptions{
+		Nodes: nodes,
+	}
+
+	cluster, err := NewCluster(opts)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	defer func() {
+		if err := cluster.Stop(); err != nil {
+			t.Error(err.Error())
+		}
+	}()
+
+	if err := cluster.Start(); err != nil {
+		t.Error(err.Error())
+	}
+
+	count := 32
+	pingChan := make(chan *PingCommand, count)
+	for i := 0; i < count; i++ {
+		logDebug("i: %d", i)
+		go func() {
+			command := &PingCommand{}
+			if err := cluster.Execute(command); err != nil {
+				t.Error(err.Error())
+			}
+			pingChan <- command
+		}()
+	}
+
+	j := 0
+	for i := 0; i < count; i++ {
+		select {
+			case pingCommand := <-pingChan:
+				logDebug("j: %d, pingCommand: %v", j, pingCommand)
+				j++
+		}
+	}
+	if expected, actual := count, j; expected != actual {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+}
