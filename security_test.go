e1acf5de41be098b513b6edff17923a14abb8791
Luke Bakken <lbakken@basho.com>
2015-07-21 14:38:30 -0700

    Add security test using a client certificate.

--- security_test.go
+++ security_test.go
@@ -9,31 +9,8 @@ import (
 	"testing"
 )
 
-func TestExecuteCommandOnClusterWithSecurity(t *testing.T) {
+func buildClusterAndRunTest(t *testing.T, nodeOptions *NodeOptions) {
 	var err error
-	var pemData []byte
-	if pemData, err = ioutil.ReadFile("./tools/test-ca/certs/cacert.pem"); err != nil {
-		t.Fatal(err.Error())
-	}
-	caCertPool := x509.NewCertPool()
-	if ok := caCertPool.AppendCertsFromPEM(pemData); !ok {
-		t.Fatal("could not append PEM cert data")
-	}
-	tlsConfig := &tls.Config{
-		ServerName:         "riak-test",
-		InsecureSkipVerify: false, // set to 'true' to not require CA certs
-		ClientCAs:          caCertPool,
-	}
-	authOptions := &AuthOptions{
-		User:      "riakpass",
-		Password:  "Test1234",
-		TlsConfig: tlsConfig,
-	}
-	nodeOptions := &NodeOptions{
-		RemoteAddress: remoteAddress,
-		AuthOptions:   authOptions,
-	}
-
 	var node *Node
 	if node, err = NewNode(nodeOptions); err != nil {
 		t.Error(err.Error())
@@ -81,4 +58,64 @@ func TestExecuteCommandOnClusterWithSecurity(t *testing.T) {
 	if expected, actual := true, command.Successful(); expected != actual {
 		t.Errorf("expected %v, got %v", expected, actual)
 	}
+}
+
+func TestExecuteCommandOnClusterWithSecurity(t *testing.T) {
+	var err error
+	var pemData []byte
+	if pemData, err = ioutil.ReadFile("./tools/test-ca/certs/cacert.pem"); err != nil {
+		t.Fatal(err.Error())
+	}
+	caCertPool := x509.NewCertPool()
+	if ok := caCertPool.AppendCertsFromPEM(pemData); !ok {
+		t.Fatal("could not append PEM cert data")
+	}
+	tlsConfig := &tls.Config{
+		ServerName:         "riak-test",
+		InsecureSkipVerify: false, // set to 'true' to not require CA certs
+		ClientCAs:          caCertPool,
+	}
+	authOptions := &AuthOptions{
+		User:      "riakpass",
+		Password:  "Test1234",
+		TlsConfig: tlsConfig,
+	}
+	nodeOptions := &NodeOptions{
+		RemoteAddress: remoteAddress,
+		AuthOptions:   authOptions,
+	}
+	buildClusterAndRunTest(t, nodeOptions)
+}
+
+func TestExecuteCommandOnClusterWithSecurityAndClientCertificate(t *testing.T) {
+	var err error
+	var caCertPemData []byte
+	if caCertPemData, err = ioutil.ReadFile("./tools/test-ca/certs/cacert.pem"); err != nil {
+		t.Fatal(err.Error())
+	}
+	var cert tls.Certificate
+	if cert, err = tls.LoadX509KeyPair(
+		"./tools/test-ca/certs/riakuser-client-cert.pem",
+		"./tools/test-ca/private/riakuser-client-cert-key.pem"); err != nil {
+		t.Fatal(err.Error())
+	}
+	caCertPool := x509.NewCertPool()
+	if ok := caCertPool.AppendCertsFromPEM(caCertPemData); !ok {
+		t.Fatal("could not append PEM cert data")
+	}
+	tlsConfig := &tls.Config{
+		ServerName:         "riak-test",
+		InsecureSkipVerify: false, // set to 'true' to not require CA certs
+		ClientCAs:          caCertPool,
+		Certificates:       []tls.Certificate{cert},
+	}
+	authOptions := &AuthOptions{
+		User:      "riakuser",
+		TlsConfig: tlsConfig,
+	}
+	nodeOptions := &NodeOptions{
+		RemoteAddress: remoteAddress,
+		AuthOptions:   authOptions,
+	}
+	buildClusterAndRunTest(t, nodeOptions)
 }
