a2789c43b59dc6710c69898fcfebed7e055968bd
Christopher Mancini <cmancini@basho.com>
2015-08-05 11:11:58 -0400

    Add comments to logging, add doc.go file for package overview, renamed state_test -> states_test so it properly imports examples when I add them

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Package riak provides the interfaces needed to interact with Riak KV using
+Protocol Buffers. Riak KV is a distributed key-value datastore designed to be
+fault tolerant, scalable, and flexible.
+
+Currently, this library was written for and tested against Riak KV 2.0+.
+*/
+package riak
--- logging.go
+++ logging.go
@@ -1,7 +1,3 @@
-// Copyright 2015 Basho Technologies, Inc. All rights reserved.
-// Use of this source code is governed by Apache License 2.0
-// license that can be found in the LICENSE file.
-
 package riak
 
 // Bare-bones logging to enable/disable debug logging
@@ -12,33 +8,40 @@ import (
 	"os"
 )
 
+// If true, debug messages will be written to the log
 var EnableDebugLogging = false
 
 var errLogger = log.New(os.Stderr, "", log.LstdFlags)
 var logger = log.New(os.Stderr, "", log.LstdFlags)
 
+// setLogWriter replaces the default log writer, which uses Stderr
 func setLogWriter(out io.Writer) {
 	logger = log.New(out, "", log.LstdFlags)
 }
 
+// logDebug writes formatted string debug messages using Printf only if debug logging is enabled
 func logDebug(format string, v ...interface{}) {
 	if EnableDebugLogging {
 		logger.Printf(format, v...)
 	}
 }
 
+// logWarnln writes string warning messages using Pringln
 func logWarnln(v string) {
 	logger.Println(v)
 }
 
+// logError writes formatted string error messages using Printf
 func logError(format string, v ...interface{}) {
 	errLogger.Printf(format, v...)
 }
 
+// logErr writes err.Error() using Println
 func logErr(err error) {
 	errLogger.Println(err.Error())
 }
 
+// logErrorln writes a string error message using Println
 func logErrorln(v string) {
 	errLogger.Println(v)
 }
