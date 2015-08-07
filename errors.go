019bd36ba6d6720e76c1fa45a78313ae71785098
Christopher Mancini <cmancini@basho.com>
2015-08-07 12:51:39 -0400

    Convert comment to be block level vs one for each var. Sort vars for easy management.

--- errors.go
+++ errors.go
@@ -2,35 +2,16 @@ package riak
 
 import errors "errors"
 
+// Convenience variables used to generate errors throughout the library
 var (
-	// ErrOptionsRequired is thrown when options are not included in a function call
-	ErrOptionsRequired = errors.New("Options are required")
-
-	// ErrAddressRequired is thrown when the RemoteAddress value is missing from the
-	// connectionOptions object when passed into the newConnection function
-	ErrAddressRequired = errors.New("RemoteAddress is required in options")
-
-	// ErrCannotRead is thrown when
-	ErrCannotRead = errors.New("Cannot read from a non-active or closed connection")
-
-	// ErrCannotWrite is thrown when
-	ErrCannotWrite = errors.New("Cannot write to a non-active or closed connection")
-
-	// ErrBucketRequired is thrown when
-	ErrBucketRequired = errors.New("Bucket is required")
-
-	// ErrKeyRequired is thrown when
-	ErrKeyRequired = errors.New("Key is required")
-
-	// ErrExpectedResponse is thrown when
-	ErrExpectedResponse = errors.New("Expected a response from Riak but did not receive one")
-
-	// ErrNilOptions is thrown when
-	ErrNilOptions = errors.New("[Command] options must be non-nil")
-
-	// ErrAuthMissingConfig is thrown when
-	ErrAuthMissingConfig = errors.New("[Connection] authentication is missing TLS config")
-
-	// ErrAuthTLSUpgradeFailed is thrown when
+	ErrAddressRequired      = errors.New("RemoteAddress is required in options")
+	ErrAuthMissingConfig    = errors.New("[Connection] authentication is missing TLS config")
 	ErrAuthTLSUpgradeFailed = errors.New("[Connection] upgrading to TLS connection failed")
+	ErrBucketRequired       = errors.New("Bucket is required")
+	ErrCannotRead           = errors.New("Cannot read from a non-active or closed connection")
+	ErrCannotWrite          = errors.New("Cannot write to a non-active or closed connection")
+	ErrExpectedResponse     = errors.New("Expected a response from Riak but did not receive one")
+	ErrKeyRequired          = errors.New("Key is required")
+	ErrNilOptions           = errors.New("[Command] options must be non-nil")
+	ErrOptionsRequired      = errors.New("Options are required")
 )
