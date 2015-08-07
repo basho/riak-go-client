01b06e2cae003d8c62cfdf5a9bf0e5c883797d6b
Luke Bakken <luke@bakken.io>
2015-08-07 05:47:42 -0700

    whitespace

--- doc.go
+++ doc.go
@@ -7,64 +7,61 @@ Currently, this library was written for and tested against Riak KV 2.0+.
 
 TL;DR;
 
-  import "fmt"
+	import "fmt"
 
-  func main() {
-    nodeOpts := &NodeOptions{
-      RemoteAddress: "127.0.0.1:8098",
-    }
+	func main() {
+		nodeOpts := &NodeOptions{
+			RemoteAddress: "127.0.0.1:8098",
+		}
 
-    var node *Node
-    var err error
-    if node, err = NewNode(nodeOpts); err != nil {
-      fmt.Println(err.Error())
-    }
+		var node *Node
+		var err error
+		if node, err = NewNode(nodeOpts); err != nil {
+			fmt.Println(err.Error())
+		}
 
-    nodes := []*Node{node}
-    opts := &ClusterOptions{
-      Nodes: nodes,
-    }
+		nodes := []*Node{node}
+		opts := &ClusterOptions{
+			Nodes: nodes,
+		}
 
-    cluster, err := NewCluster(opts)
-    if err != nil {
-      fmt.Println(err.Error())
-    }
+		cluster, err := NewCluster(opts)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
 
-    defer func() {
-      if err := cluster.Stop(); err != nil {
-        fmt.Println(err.Error())
-      }
-    }()
+		defer func() {
+			if err := cluster.Stop(); err != nil {
+				fmt.Println(err.Error())
+			}
+		}()
 
-    if err := cluster.Start(); err != nil {
-      fmt.Println(err.Error())
-    }
+		if err := cluster.Start(); err != nil {
+			fmt.Println(err.Error())
+		}
 
-    obj := &Object{
-  		ContentType:     "text/plain",
-  		Charset:         "utf-8",
-  		ContentEncoding: "utf-8",
-  		Value:           []byte("this is a value in Riak"),
-  	}
+		obj := &Object{
+			ContentType:     "text/plain",
+			Charset:         "utf-8",
+			ContentEncoding: "utf-8",
+			Value:           []byte("this is a value in Riak"),
+		}
 
-    cmd, err := NewStoreValueCommandBuilder().
-  		WithBucket(testBucketName).
-  		WithContent(obj).
-  		Build()
-    if err != nil {
-  		fmt.Println(err.Error())
-  	}
+		cmd, err := NewStoreValueCommandBuilder().
+			WithBucket(testBucketName).
+			WithContent(obj).
+			Build()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
 
-  	if err := cluster.Execute(cmd); err != nil {
-  		fmt.Println(err.Error())
-  	}
+		if err := cluster.Execute(cmd); err != nil {
+			fmt.Println(err.Error())
+		}
 
-  	if svc, ok := cmd.(*StoreValueCommand); ok {
-  		rsp := svc.Response
-      fmt.Println(rsp.GeneratedKey)
-  	} else {
-  		fmt.Println(err.Error())
-  	}
-  }
+		svc := cmd.(*StoreValueCommand)
+		rsp := svc.Response
+		fmt.Println(rsp.GeneratedKey)
+	}
 */
 package riak
