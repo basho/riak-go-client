a941bf84264303adcfe7bf9aa4ac6a16f35a601d
Luke Bakken <lbakken@basho.com>
2015-08-07 06:12:44 -0700

    Remove copyright statements.

--- crdt_commands.go
+++ crdt_commands.go
@@ -1,7 +1,3 @@
-// Copyright 2015 Basho Technologies, Inc. All rights reserved.
-// Use of this source code is governed by Apache License 2.0
-// license that can be found in the LICENSE file.
-
 package riak
 
 import (
