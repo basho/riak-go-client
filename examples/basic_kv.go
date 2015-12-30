76b7964f86793cc10840c6d96e31b59992282267
Luke Bakken <lbakken@basho.com>
2015-12-29 16:33:19 -0800

    Add more examples

--- examples/basic_kv.go
+++ examples/basic_kv.go
@@ -6,12 +6,13 @@ import (
 	riak "github.com/basho/riak-go-client"
 )
 
-// make sure bucket-type 'animals' is created on Riak
+// make sure these bucket-types are created:
+// 'animals', 'quotes', 'sports', 'cars', 'users', 'n_val_of_5'
 func main() {
 	//riak.EnableDebugLogging = true
 
 	nodeOpts := &riak.NodeOptions{
-		RemoteAddress: "riak-test:8087",
+		RemoteAddress: "riak-test:10017",
 	}
 
 	var node *riak.Node
@@ -52,6 +53,10 @@ func main() {
 	storeRufus(cluster)
 	storeStray(cluster)
 	fetchRufusWithR(cluster)
+	storeQuote(cluster)
+	storeAndUpdateSport(cluster)
+	storeCar(cluster)
+	storeUserThenDelete(cluster)
 }
 
 func storeRufus(cluster *riak.Cluster) {
@@ -84,6 +89,164 @@ func storeRufus(cluster *riak.Cluster) {
 	fmt.Println(rsp.VClock)
 }
 
+func storeQuote(cluster *riak.Cluster) {
+	obj := &riak.Object{
+		ContentType:     "text/plain",
+		Charset:         "utf-8",
+		ContentEncoding: "utf-8",
+		Value:           []byte("I have nothing to declare but my genius"),
+	}
+
+	cmd, err := riak.NewStoreValueCommandBuilder().
+		WithBucketType("quotes").
+		WithBucket("oscar_wilde").
+		WithKey("genius").
+		WithContent(obj).
+		Build()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err := cluster.Execute(cmd); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	svc := cmd.(*riak.StoreValueCommand)
+	rsp := svc.Response
+	fmt.Println(rsp.VClock)
+}
+
+func storeCar(cluster *riak.Cluster) {
+	obj := &riak.Object{
+		ContentType:     "text/plain",
+		Charset:         "utf-8",
+		ContentEncoding: "utf-8",
+		Value:           []byte("vroom"),
+	}
+
+	cmd, err := riak.NewStoreValueCommandBuilder().
+		WithBucketType("cars").
+		WithBucket("dodge").
+		WithKey("viper").
+		WithW(3).
+		WithContent(obj).
+		WithReturnBody(true).
+		Build()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err := cluster.Execute(cmd); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func storeUserThenDelete(cluster *riak.Cluster) {
+	obj := &riak.Object{
+		ContentType:     "application/json",
+		Charset:         "utf-8",
+		ContentEncoding: "utf-8",
+		Value:           []byte("{'user':'data'}"),
+	}
+
+	cmd, err := riak.NewStoreValueCommandBuilder().
+		WithBucketType("users").
+		WithBucket("random_user_keys").
+		WithContent(obj).
+		Build()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err := cluster.Execute(cmd); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	svc := cmd.(*riak.StoreValueCommand)
+	rsp := svc.Response
+	fmt.Printf("Generated key: %v\n", rsp.GeneratedKey)
+
+	cmd, err = riak.NewDeleteValueCommandBuilder().
+		WithBucketType("users").
+		WithBucket("random_user_keys").
+		WithKey(rsp.GeneratedKey).
+		Build()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err := cluster.Execute(cmd); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func storeAndUpdateSport(cluster *riak.Cluster) {
+	obj := &riak.Object{
+		ContentType:     "text/plain",
+		Charset:         "utf-8",
+		ContentEncoding: "utf-8",
+		Value:           []byte("Washington Generals"),
+	}
+
+	cmd, err := riak.NewStoreValueCommandBuilder().
+		WithBucketType("sports").
+		WithBucket("nba").
+		WithKey("champion").
+		WithContent(obj).
+		WithReturnBody(true).
+		Build()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err := cluster.Execute(cmd); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	svc := cmd.(*riak.StoreValueCommand)
+	rsp := svc.Response
+	obj = rsp.Values[0]
+	obj.Value = []byte("Harlem Globetrotters")
+
+	cmd, err = riak.NewStoreValueCommandBuilder().
+		WithBucketType("sports").
+		WithBucket("nba").
+		WithKey("champion").
+		WithContent(obj).
+		WithReturnBody(true).
+		Build()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err := cluster.Execute(cmd); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	svc = cmd.(*riak.StoreValueCommand)
+	rsp = svc.Response
+	obj = rsp.Values[0]
+	fmt.Printf("champion: %v\n", string(obj.Value))
+}
+
 func fetchRufus(cluster *riak.Cluster) {
 	cmd, err := riak.NewFetchValueCommandBuilder().
 		WithBucketType("animals").
@@ -101,8 +264,8 @@ func fetchRufus(cluster *riak.Cluster) {
 		return
 	}
 
-	svc := cmd.(*riak.FetchValueCommand)
-	rsp := svc.Response
+	fvc := cmd.(*riak.FetchValueCommand)
+	rsp := fvc.Response
 	fmt.Println(rsp.IsNotFound)
 }
 
@@ -156,8 +319,8 @@ func fetchRufusWithR(cluster *riak.Cluster) {
 		return
 	}
 
-	svc := cmd.(*riak.FetchValueCommand)
-	rsp := svc.Response
+	fvc := cmd.(*riak.FetchValueCommand)
+	rsp := fvc.Response
 	fmt.Println(rsp.IsNotFound)
 }
 
@@ -178,8 +341,8 @@ func fetchChampion(cluster *riak.Cluster) {
 		return
 	}
 
-	svc := cmd.(*riak.FetchValueCommand)
-	rsp := svc.Response
+	fvc := cmd.(*riak.FetchValueCommand)
+	rsp := fvc.Response
 	var obj *riak.Object
 	if len(rsp.Values) > 0 {
 		obj = rsp.Values[0]
