1ab5261e1f95f795ae57e4a0db9b5921a6fa0e9a
Christopher Mancini <cmancini@basho.com>
2016-04-28 14:04:49 -0400

    had to add empty single quotes to get sed to work on mac to ignore backup files. regenerated pb structs based on proto files for tag 2.1.2.0

--- rpb/riak/riak.pb.go
+++ rpb/riak/riak.pb.go
@@ -22,15 +22,23 @@ It has these top-level messages:
 	RpbCommitHook
 	RpbBucketProps
 	RpbAuthReq
+	RpbToggleEncodingReq
+	RpbToggleEncodingResp
 */
 package riak
 
 import proto "github.com/golang/protobuf/proto"
+import fmt "fmt"
 import math "math"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
+var _ = fmt.Errorf
 var _ = math.Inf
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the proto package it is being compiled against.
+const _ = proto.ProtoPackageIsVersion1
 
 // Used by riak_repl bucket fixup
 type RpbBucketProps_RpbReplMode int32
@@ -71,6 +79,9 @@ func (x *RpbBucketProps_RpbReplMode) UnmarshalJSON(data []byte) error {
 	*x = RpbBucketProps_RpbReplMode(value)
 	return nil
 }
+func (RpbBucketProps_RpbReplMode) EnumDescriptor() ([]byte, []int) {
+	return fileDescriptor0, []int{11, 0}
+}
 
 // Error response - may be generated for any Req
 type RpbErrorResp struct {
@@ -79,9 +90,10 @@ type RpbErrorResp struct {
 	XXX_unrecognized []byte  `json:"-"`
 }
 
-func (m *RpbErrorResp) Reset()         { *m = RpbErrorResp{} }
-func (m *RpbErrorResp) String() string { return proto.CompactTextString(m) }
-func (*RpbErrorResp) ProtoMessage()    {}
+func (m *RpbErrorResp) Reset()                    { *m = RpbErrorResp{} }
+func (m *RpbErrorResp) String() string            { return proto.CompactTextString(m) }
+func (*RpbErrorResp) ProtoMessage()               {}
+func (*RpbErrorResp) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
 func (m *RpbErrorResp) GetErrmsg() []byte {
 	if m != nil {
@@ -104,9 +116,10 @@ type RpbGetServerInfoResp struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbGetServerInfoResp) Reset()         { *m = RpbGetServerInfoResp{} }
-func (m *RpbGetServerInfoResp) String() string { return proto.CompactTextString(m) }
-func (*RpbGetServerInfoResp) ProtoMessage()    {}
+func (m *RpbGetServerInfoResp) Reset()                    { *m = RpbGetServerInfoResp{} }
+func (m *RpbGetServerInfoResp) String() string            { return proto.CompactTextString(m) }
+func (*RpbGetServerInfoResp) ProtoMessage()               {}
+func (*RpbGetServerInfoResp) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
 func (m *RpbGetServerInfoResp) GetNode() []byte {
 	if m != nil {
@@ -129,9 +142,10 @@ type RpbPair struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbPair) Reset()         { *m = RpbPair{} }
-func (m *RpbPair) String() string { return proto.CompactTextString(m) }
-func (*RpbPair) ProtoMessage()    {}
+func (m *RpbPair) Reset()                    { *m = RpbPair{} }
+func (m *RpbPair) String() string            { return proto.CompactTextString(m) }
+func (*RpbPair) ProtoMessage()               {}
+func (*RpbPair) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
 func (m *RpbPair) GetKey() []byte {
 	if m != nil {
@@ -154,9 +168,10 @@ type RpbGetBucketReq struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbGetBucketReq) Reset()         { *m = RpbGetBucketReq{} }
-func (m *RpbGetBucketReq) String() string { return proto.CompactTextString(m) }
-func (*RpbGetBucketReq) ProtoMessage()    {}
+func (m *RpbGetBucketReq) Reset()                    { *m = RpbGetBucketReq{} }
+func (m *RpbGetBucketReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbGetBucketReq) ProtoMessage()               {}
+func (*RpbGetBucketReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
 
 func (m *RpbGetBucketReq) GetBucket() []byte {
 	if m != nil {
@@ -178,9 +193,10 @@ type RpbGetBucketResp struct {
 	XXX_unrecognized []byte          `json:"-"`
 }
 
-func (m *RpbGetBucketResp) Reset()         { *m = RpbGetBucketResp{} }
-func (m *RpbGetBucketResp) String() string { return proto.CompactTextString(m) }
-func (*RpbGetBucketResp) ProtoMessage()    {}
+func (m *RpbGetBucketResp) Reset()                    { *m = RpbGetBucketResp{} }
+func (m *RpbGetBucketResp) String() string            { return proto.CompactTextString(m) }
+func (*RpbGetBucketResp) ProtoMessage()               {}
+func (*RpbGetBucketResp) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{4} }
 
 func (m *RpbGetBucketResp) GetProps() *RpbBucketProps {
 	if m != nil {
@@ -197,9 +213,10 @@ type RpbSetBucketReq struct {
 	XXX_unrecognized []byte          `json:"-"`
 }
 
-func (m *RpbSetBucketReq) Reset()         { *m = RpbSetBucketReq{} }
-func (m *RpbSetBucketReq) String() string { return proto.CompactTextString(m) }
-func (*RpbSetBucketReq) ProtoMessage()    {}
+func (m *RpbSetBucketReq) Reset()                    { *m = RpbSetBucketReq{} }
+func (m *RpbSetBucketReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbSetBucketReq) ProtoMessage()               {}
+func (*RpbSetBucketReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{5} }
 
 func (m *RpbSetBucketReq) GetBucket() []byte {
 	if m != nil {
@@ -229,9 +246,10 @@ type RpbResetBucketReq struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbResetBucketReq) Reset()         { *m = RpbResetBucketReq{} }
-func (m *RpbResetBucketReq) String() string { return proto.CompactTextString(m) }
-func (*RpbResetBucketReq) ProtoMessage()    {}
+func (m *RpbResetBucketReq) Reset()                    { *m = RpbResetBucketReq{} }
+func (m *RpbResetBucketReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbResetBucketReq) ProtoMessage()               {}
+func (*RpbResetBucketReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{6} }
 
 func (m *RpbResetBucketReq) GetBucket() []byte {
 	if m != nil {
@@ -253,9 +271,10 @@ type RpbGetBucketTypeReq struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbGetBucketTypeReq) Reset()         { *m = RpbGetBucketTypeReq{} }
-func (m *RpbGetBucketTypeReq) String() string { return proto.CompactTextString(m) }
-func (*RpbGetBucketTypeReq) ProtoMessage()    {}
+func (m *RpbGetBucketTypeReq) Reset()                    { *m = RpbGetBucketTypeReq{} }
+func (m *RpbGetBucketTypeReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbGetBucketTypeReq) ProtoMessage()               {}
+func (*RpbGetBucketTypeReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{7} }
 
 func (m *RpbGetBucketTypeReq) GetType() []byte {
 	if m != nil {
@@ -271,9 +290,10 @@ type RpbSetBucketTypeReq struct {
 	XXX_unrecognized []byte          `json:"-"`
 }
 
-func (m *RpbSetBucketTypeReq) Reset()         { *m = RpbSetBucketTypeReq{} }
-func (m *RpbSetBucketTypeReq) String() string { return proto.CompactTextString(m) }
-func (*RpbSetBucketTypeReq) ProtoMessage()    {}
+func (m *RpbSetBucketTypeReq) Reset()                    { *m = RpbSetBucketTypeReq{} }
+func (m *RpbSetBucketTypeReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbSetBucketTypeReq) ProtoMessage()               {}
+func (*RpbSetBucketTypeReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{8} }
 
 func (m *RpbSetBucketTypeReq) GetType() []byte {
 	if m != nil {
@@ -297,9 +317,10 @@ type RpbModFun struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbModFun) Reset()         { *m = RpbModFun{} }
-func (m *RpbModFun) String() string { return proto.CompactTextString(m) }
-func (*RpbModFun) ProtoMessage()    {}
+func (m *RpbModFun) Reset()                    { *m = RpbModFun{} }
+func (m *RpbModFun) String() string            { return proto.CompactTextString(m) }
+func (*RpbModFun) ProtoMessage()               {}
+func (*RpbModFun) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{9} }
 
 func (m *RpbModFun) GetModule() []byte {
 	if m != nil {
@@ -323,9 +344,10 @@ type RpbCommitHook struct {
 	XXX_unrecognized []byte     `json:"-"`
 }
 
-func (m *RpbCommitHook) Reset()         { *m = RpbCommitHook{} }
-func (m *RpbCommitHook) String() string { return proto.CompactTextString(m) }
-func (*RpbCommitHook) ProtoMessage()    {}
+func (m *RpbCommitHook) Reset()                    { *m = RpbCommitHook{} }
+func (m *RpbCommitHook) String() string            { return proto.CompactTextString(m) }
+func (*RpbCommitHook) ProtoMessage()               {}
+func (*RpbCommitHook) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{10} }
 
 func (m *RpbCommitHook) GetModfun() *RpbModFun {
 	if m != nil {
@@ -382,9 +404,10 @@ type RpbBucketProps struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbBucketProps) Reset()         { *m = RpbBucketProps{} }
-func (m *RpbBucketProps) String() string { return proto.CompactTextString(m) }
-func (*RpbBucketProps) ProtoMessage()    {}
+func (m *RpbBucketProps) Reset()                    { *m = RpbBucketProps{} }
+func (m *RpbBucketProps) String() string            { return proto.CompactTextString(m) }
+func (*RpbBucketProps) ProtoMessage()               {}
+func (*RpbBucketProps) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{11} }
 
 const Default_RpbBucketProps_HasPrecommit bool = false
 const Default_RpbBucketProps_HasPostcommit bool = false
@@ -592,9 +615,10 @@ type RpbAuthReq struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbAuthReq) Reset()         { *m = RpbAuthReq{} }
-func (m *RpbAuthReq) String() string { return proto.CompactTextString(m) }
-func (*RpbAuthReq) ProtoMessage()    {}
+func (m *RpbAuthReq) Reset()                    { *m = RpbAuthReq{} }
+func (m *RpbAuthReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbAuthReq) ProtoMessage()               {}
+func (*RpbAuthReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{12} }
 
 func (m *RpbAuthReq) GetUser() []byte {
 	if m != nil {
@@ -610,6 +634,108 @@ func (m *RpbAuthReq) GetPassword() []byte {
 	return nil
 }
 
+// Erlang term encoding
+type RpbToggleEncodingReq struct {
+	UseNative        *bool  `protobuf:"varint,1,req,name=use_native" json:"use_native,omitempty"`
+	XXX_unrecognized []byte `json:"-"`
+}
+
+func (m *RpbToggleEncodingReq) Reset()                    { *m = RpbToggleEncodingReq{} }
+func (m *RpbToggleEncodingReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbToggleEncodingReq) ProtoMessage()               {}
+func (*RpbToggleEncodingReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{13} }
+
+func (m *RpbToggleEncodingReq) GetUseNative() bool {
+	if m != nil && m.UseNative != nil {
+		return *m.UseNative
+	}
+	return false
+}
+
+type RpbToggleEncodingResp struct {
+	UseNative        *bool  `protobuf:"varint,1,req,name=use_native" json:"use_native,omitempty"`
+	XXX_unrecognized []byte `json:"-"`
+}
+
+func (m *RpbToggleEncodingResp) Reset()                    { *m = RpbToggleEncodingResp{} }
+func (m *RpbToggleEncodingResp) String() string            { return proto.CompactTextString(m) }
+func (*RpbToggleEncodingResp) ProtoMessage()               {}
+func (*RpbToggleEncodingResp) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{14} }
+
+func (m *RpbToggleEncodingResp) GetUseNative() bool {
+	if m != nil && m.UseNative != nil {
+		return *m.UseNative
+	}
+	return false
+}
+
 func init() {
+	proto.RegisterType((*RpbErrorResp)(nil), "RpbErrorResp")
+	proto.RegisterType((*RpbGetServerInfoResp)(nil), "RpbGetServerInfoResp")
+	proto.RegisterType((*RpbPair)(nil), "RpbPair")
+	proto.RegisterType((*RpbGetBucketReq)(nil), "RpbGetBucketReq")
+	proto.RegisterType((*RpbGetBucketResp)(nil), "RpbGetBucketResp")
+	proto.RegisterType((*RpbSetBucketReq)(nil), "RpbSetBucketReq")
+	proto.RegisterType((*RpbResetBucketReq)(nil), "RpbResetBucketReq")
+	proto.RegisterType((*RpbGetBucketTypeReq)(nil), "RpbGetBucketTypeReq")
+	proto.RegisterType((*RpbSetBucketTypeReq)(nil), "RpbSetBucketTypeReq")
+	proto.RegisterType((*RpbModFun)(nil), "RpbModFun")
+	proto.RegisterType((*RpbCommitHook)(nil), "RpbCommitHook")
+	proto.RegisterType((*RpbBucketProps)(nil), "RpbBucketProps")
+	proto.RegisterType((*RpbAuthReq)(nil), "RpbAuthReq")
+	proto.RegisterType((*RpbToggleEncodingReq)(nil), "RpbToggleEncodingReq")
+	proto.RegisterType((*RpbToggleEncodingResp)(nil), "RpbToggleEncodingResp")
 	proto.RegisterEnum("RpbBucketProps_RpbReplMode", RpbBucketProps_RpbReplMode_name, RpbBucketProps_RpbReplMode_value)
+}
+
+var fileDescriptor0 = []byte{
+	// 761 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0x8c, 0x54, 0x59, 0x6f, 0xe2, 0x48,
+	0x10, 0x5e, 0xae, 0x00, 0xc5, 0x19, 0xe7, 0xea, 0x4d, 0xb2, 0x2b, 0xe2, 0xd5, 0x4a, 0x7b, 0x12,
+	0x2d, 0x6f, 0x3b, 0x1a, 0x8d, 0x14, 0x22, 0x32, 0x13, 0x89, 0xcc, 0x44, 0x40, 0x1e, 0xe6, 0xc9,
+	0x32, 0x76, 0x03, 0x16, 0x76, 0xb7, 0xd3, 0xdd, 0xc6, 0x93, 0x7f, 0x35, 0x3f, 0x71, 0xca, 0x0d,
+	0x86, 0x1c, 0x28, 0x33, 0x0f, 0x96, 0xdc, 0x55, 0xdf, 0x57, 0xf5, 0x75, 0x55, 0x75, 0x01, 0x08,
+	0xcf, 0x9e, 0xb7, 0x43, 0xc1, 0x15, 0x37, 0xcf, 0xa1, 0x3a, 0x08, 0xc7, 0x3d, 0x21, 0xb8, 0x18,
+	0x50, 0x19, 0x1a, 0x75, 0xd8, 0xa1, 0x42, 0x04, 0x72, 0x4a, 0x32, 0xad, 0xec, 0x1f, 0x55, 0xa3,
+	0x01, 0x45, 0x3c, 0x3b, 0xdc, 0xa5, 0x24, 0x8b, 0x86, 0x9a, 0xf9, 0x16, 0xf6, 0x91, 0xf0, 0x9e,
+	0xaa, 0x21, 0x15, 0x0b, 0x2a, 0xae, 0xd9, 0x84, 0x6b, 0x62, 0x15, 0xf2, 0x2c, 0x41, 0x65, 0x5a,
+	0x19, 0xa4, 0x1d, 0x42, 0x5d, 0x6a, 0xbf, 0x85, 0x9f, 0xf4, 0x38, 0x43, 0x36, 0xda, 0xcd, 0xdf,
+	0xa1, 0x88, 0xec, 0x5b, 0xdb, 0x13, 0x46, 0x05, 0x72, 0x73, 0xfa, 0xb0, 0x4a, 0x53, 0x83, 0xc2,
+	0xc2, 0xf6, 0x23, 0xba, 0x82, 0x9d, 0x43, 0x63, 0x99, 0xa4, 0x1b, 0x39, 0x73, 0xaa, 0x06, 0xf4,
+	0x3e, 0x11, 0x36, 0xd6, 0x87, 0x15, 0x03, 0xf3, 0xa9, 0x87, 0x30, 0x25, 0x74, 0xa0, 0xf9, 0x94,
+	0x80, 0x8a, 0x7e, 0x85, 0x02, 0xde, 0x31, 0x94, 0x9a, 0x50, 0xe9, 0x34, 0xda, 0x88, 0x58, 0xba,
+	0x6f, 0x13, 0xb3, 0xf9, 0x49, 0x27, 0x19, 0xbe, 0x96, 0x64, 0x1d, 0x22, 0xbb, 0x35, 0xc4, 0x5a,
+	0x44, 0x4e, 0x8b, 0xf8, 0x0f, 0x76, 0xd1, 0x8f, 0xb9, 0x7f, 0x5c, 0xf7, 0x6f, 0xb0, 0xf7, 0x58,
+	0xf7, 0x08, 0x3d, 0x09, 0x29, 0x05, 0x69, 0x8a, 0x79, 0xa9, 0x41, 0xc3, 0x57, 0x41, 0xdf, 0x93,
+	0x6a, 0xfe, 0x0b, 0x65, 0xb4, 0xdc, 0x70, 0xf7, 0x2a, 0x62, 0x89, 0xa8, 0x80, 0xbb, 0x91, 0x9f,
+	0x92, 0x9b, 0x50, 0x9a, 0x44, 0xcc, 0x51, 0xcb, 0x46, 0x25, 0x39, 0xff, 0x87, 0x1a, 0xc2, 0x2f,
+	0x79, 0x10, 0x78, 0xea, 0x03, 0xe7, 0x73, 0xe3, 0x58, 0x53, 0x10, 0xa5, 0x3b, 0x5c, 0xe9, 0x40,
+	0x7b, 0x13, 0x2e, 0xe9, 0xbd, 0x1d, 0xa4, 0x77, 0xfa, 0x5a, 0x80, 0xfa, 0xb3, 0x3a, 0x61, 0x7b,
+	0x99, 0x85, 0x0d, 0xd6, 0xdc, 0x9a, 0x61, 0x00, 0xd8, 0xbe, 0xcf, 0x63, 0x2b, 0x88, 0x7c, 0xa5,
+	0x59, 0x25, 0xe3, 0x08, 0x1a, 0xbe, 0x2d, 0x95, 0x15, 0x0b, 0x4f, 0x51, 0x2b, 0xf6, 0x98, 0xd4,
+	0x55, 0x2d, 0x19, 0x67, 0x50, 0x0e, 0x05, 0x75, 0xb4, 0x12, 0x92, 0x6f, 0xe5, 0x30, 0x77, 0xbd,
+	0xfd, 0x54, 0xdb, 0x29, 0xd4, 0x66, 0xb6, 0xb4, 0x36, 0xb0, 0x42, 0xc2, 0x7c, 0x53, 0x98, 0xd8,
+	0xbe, 0xa4, 0x86, 0x09, 0x10, 0x72, 0xa9, 0x56, 0xae, 0x9d, 0xad, 0x11, 0x7e, 0x81, 0xba, 0x8e,
+	0xb0, 0xc1, 0x15, 0x1f, 0x87, 0x68, 0x41, 0xd5, 0x41, 0xff, 0xcc, 0xc2, 0x89, 0x4d, 0x4a, 0x50,
+	0x7a, 0x51, 0x82, 0x13, 0x28, 0xfa, 0x1e, 0x9b, 0x27, 0xce, 0xf2, 0x0b, 0x27, 0xde, 0x97, 0xfb,
+	0xae, 0xb5, 0x70, 0x7c, 0xee, 0xcc, 0x09, 0xe8, 0x1a, 0xec, 0x43, 0xf5, 0x81, 0x47, 0x6c, 0x9a,
+	0x5a, 0x2b, 0x69, 0x65, 0xc6, 0xde, 0xda, 0x56, 0x4d, 0x91, 0x32, 0xc0, 0x7a, 0xa5, 0xd6, 0x9a,
+	0xb6, 0x02, 0x64, 0x43, 0x41, 0xea, 0xfa, 0xbf, 0x0c, 0x19, 0x41, 0x1a, 0xe9, 0x6f, 0x4c, 0x9a,
+	0x6b, 0x44, 0x4c, 0x76, 0xd3, 0x7f, 0x37, 0x26, 0x46, 0xfa, 0x2f, 0x62, 0xb2, 0x97, 0xc6, 0x1e,
+	0xdb, 0xd2, 0x73, 0xac, 0xfb, 0x88, 0x8b, 0x28, 0x20, 0xfb, 0xba, 0xe4, 0x7b, 0x50, 0x61, 0x5c,
+	0x4d, 0x50, 0x9e, 0x6b, 0xf1, 0x39, 0x39, 0xd0, 0x46, 0xdc, 0x04, 0x63, 0x1b, 0x5b, 0xca, 0x5c,
+	0x72, 0xa8, 0xdf, 0x38, 0x0e, 0x91, 0xa4, 0xb6, 0x70, 0x66, 0xe4, 0x48, 0x03, 0xfe, 0x84, 0xbc,
+	0xa0, 0xa1, 0x4f, 0x08, 0x9e, 0xea, 0x9d, 0x93, 0x67, 0x03, 0xd8, 0xd6, 0x4f, 0x23, 0xf4, 0xb1,
+	0x24, 0x54, 0x5f, 0x49, 0x53, 0x2d, 0x8f, 0xb9, 0xf4, 0x0b, 0xf9, 0x59, 0x07, 0xc4, 0x29, 0x74,
+	0x6d, 0x65, 0xeb, 0xa1, 0x3e, 0xd6, 0x16, 0xd4, 0xea, 0x70, 0x26, 0x3d, 0xa9, 0x28, 0x53, 0xe4,
+	0x44, 0xa7, 0x41, 0xdb, 0x72, 0x46, 0x38, 0x73, 0x28, 0x39, 0x4d, 0x6c, 0xe6, 0x3b, 0xa8, 0x3c,
+	0x0e, 0x5f, 0x86, 0xc2, 0xd5, 0x45, 0x7f, 0xd8, 0x6b, 0xfe, 0x84, 0xa3, 0x59, 0x1a, 0xf4, 0x2e,
+	0xfa, 0xa3, 0xeb, 0x9b, 0x5e, 0x33, 0x93, 0x9c, 0xae, 0xee, 0xfa, 0xfd, 0xe1, 0xe7, 0x8f, 0x97,
+	0xcd, 0xac, 0x51, 0x82, 0xfc, 0x68, 0x70, 0xd7, 0x6b, 0xe6, 0xcc, 0x7f, 0x00, 0x90, 0x7f, 0x11,
+	0xa9, 0xd9, 0xea, 0x61, 0x45, 0xb8, 0xbd, 0x36, 0x6f, 0x23, 0xb4, 0xa5, 0x8c, 0xb9, 0x70, 0x57,
+	0x6f, 0xe3, 0x2f, 0xbd, 0x02, 0x47, 0x7c, 0x3a, 0xf5, 0x69, 0x8f, 0xe1, 0x6e, 0xf4, 0xd8, 0x34,
+	0xe1, 0xa1, 0x32, 0xe4, 0x59, 0xcc, 0x56, 0xde, 0x62, 0xf9, 0xb2, 0x4a, 0xe6, 0xdf, 0x70, 0xb0,
+	0x05, 0x8b, 0xdb, 0x69, 0x0b, 0xb8, 0x7b, 0x06, 0x47, 0x38, 0x7d, 0x6d, 0xec, 0xc8, 0x8c, 0xb7,
+	0x37, 0x4b, 0x7a, 0x1c, 0x4d, 0xba, 0x3b, 0x03, 0x3c, 0xde, 0x76, 0xbf, 0x05, 0x00, 0x00, 0xff,
+	0xff, 0xa8, 0x77, 0x00, 0x9e, 0xbc, 0x05, 0x00, 0x00,
 }
--- rpb/riak_yokozuna/riak_yokozuna.pb.go
+++ rpb/riak_yokozuna/riak_yokozuna.pb.go
@@ -22,11 +22,17 @@ It has these top-level messages:
 package riak_yokozuna
 
 import proto "github.com/golang/protobuf/proto"
+import fmt "fmt"
 import math "math"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
+var _ = fmt.Errorf
 var _ = math.Inf
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the proto package it is being compiled against.
+const _ = proto.ProtoPackageIsVersion1
 
 type RpbYokozunaIndex struct {
 	Name             []byte  `protobuf:"bytes,1,req,name=name" json:"name,omitempty"`
@@ -35,9 +41,10 @@ type RpbYokozunaIndex struct {
 	XXX_unrecognized []byte  `json:"-"`
 }
 
-func (m *RpbYokozunaIndex) Reset()         { *m = RpbYokozunaIndex{} }
-func (m *RpbYokozunaIndex) String() string { return proto.CompactTextString(m) }
-func (*RpbYokozunaIndex) ProtoMessage()    {}
+func (m *RpbYokozunaIndex) Reset()                    { *m = RpbYokozunaIndex{} }
+func (m *RpbYokozunaIndex) String() string            { return proto.CompactTextString(m) }
+func (*RpbYokozunaIndex) ProtoMessage()               {}
+func (*RpbYokozunaIndex) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
 func (m *RpbYokozunaIndex) GetName() []byte {
 	if m != nil {
@@ -66,9 +73,10 @@ type RpbYokozunaIndexGetReq struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbYokozunaIndexGetReq) Reset()         { *m = RpbYokozunaIndexGetReq{} }
-func (m *RpbYokozunaIndexGetReq) String() string { return proto.CompactTextString(m) }
-func (*RpbYokozunaIndexGetReq) ProtoMessage()    {}
+func (m *RpbYokozunaIndexGetReq) Reset()                    { *m = RpbYokozunaIndexGetReq{} }
+func (m *RpbYokozunaIndexGetReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbYokozunaIndexGetReq) ProtoMessage()               {}
+func (*RpbYokozunaIndexGetReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
 func (m *RpbYokozunaIndexGetReq) GetName() []byte {
 	if m != nil {
@@ -82,9 +90,10 @@ type RpbYokozunaIndexGetResp struct {
 	XXX_unrecognized []byte              `json:"-"`
 }
 
-func (m *RpbYokozunaIndexGetResp) Reset()         { *m = RpbYokozunaIndexGetResp{} }
-func (m *RpbYokozunaIndexGetResp) String() string { return proto.CompactTextString(m) }
-func (*RpbYokozunaIndexGetResp) ProtoMessage()    {}
+func (m *RpbYokozunaIndexGetResp) Reset()                    { *m = RpbYokozunaIndexGetResp{} }
+func (m *RpbYokozunaIndexGetResp) String() string            { return proto.CompactTextString(m) }
+func (*RpbYokozunaIndexGetResp) ProtoMessage()               {}
+func (*RpbYokozunaIndexGetResp) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
 func (m *RpbYokozunaIndexGetResp) GetIndex() []*RpbYokozunaIndex {
 	if m != nil {
@@ -100,9 +109,10 @@ type RpbYokozunaIndexPutReq struct {
 	XXX_unrecognized []byte            `json:"-"`
 }
 
-func (m *RpbYokozunaIndexPutReq) Reset()         { *m = RpbYokozunaIndexPutReq{} }
-func (m *RpbYokozunaIndexPutReq) String() string { return proto.CompactTextString(m) }
-func (*RpbYokozunaIndexPutReq) ProtoMessage()    {}
+func (m *RpbYokozunaIndexPutReq) Reset()                    { *m = RpbYokozunaIndexPutReq{} }
+func (m *RpbYokozunaIndexPutReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbYokozunaIndexPutReq) ProtoMessage()               {}
+func (*RpbYokozunaIndexPutReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
 
 func (m *RpbYokozunaIndexPutReq) GetIndex() *RpbYokozunaIndex {
 	if m != nil {
@@ -124,9 +134,10 @@ type RpbYokozunaIndexDeleteReq struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbYokozunaIndexDeleteReq) Reset()         { *m = RpbYokozunaIndexDeleteReq{} }
-func (m *RpbYokozunaIndexDeleteReq) String() string { return proto.CompactTextString(m) }
-func (*RpbYokozunaIndexDeleteReq) ProtoMessage()    {}
+func (m *RpbYokozunaIndexDeleteReq) Reset()                    { *m = RpbYokozunaIndexDeleteReq{} }
+func (m *RpbYokozunaIndexDeleteReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbYokozunaIndexDeleteReq) ProtoMessage()               {}
+func (*RpbYokozunaIndexDeleteReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{4} }
 
 func (m *RpbYokozunaIndexDeleteReq) GetName() []byte {
 	if m != nil {
@@ -141,9 +152,10 @@ type RpbYokozunaSchema struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbYokozunaSchema) Reset()         { *m = RpbYokozunaSchema{} }
-func (m *RpbYokozunaSchema) String() string { return proto.CompactTextString(m) }
-func (*RpbYokozunaSchema) ProtoMessage()    {}
+func (m *RpbYokozunaSchema) Reset()                    { *m = RpbYokozunaSchema{} }
+func (m *RpbYokozunaSchema) String() string            { return proto.CompactTextString(m) }
+func (*RpbYokozunaSchema) ProtoMessage()               {}
+func (*RpbYokozunaSchema) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{5} }
 
 func (m *RpbYokozunaSchema) GetName() []byte {
 	if m != nil {
@@ -165,9 +177,10 @@ type RpbYokozunaSchemaPutReq struct {
 	XXX_unrecognized []byte             `json:"-"`
 }
 
-func (m *RpbYokozunaSchemaPutReq) Reset()         { *m = RpbYokozunaSchemaPutReq{} }
-func (m *RpbYokozunaSchemaPutReq) String() string { return proto.CompactTextString(m) }
-func (*RpbYokozunaSchemaPutReq) ProtoMessage()    {}
+func (m *RpbYokozunaSchemaPutReq) Reset()                    { *m = RpbYokozunaSchemaPutReq{} }
+func (m *RpbYokozunaSchemaPutReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbYokozunaSchemaPutReq) ProtoMessage()               {}
+func (*RpbYokozunaSchemaPutReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{6} }
 
 func (m *RpbYokozunaSchemaPutReq) GetSchema() *RpbYokozunaSchema {
 	if m != nil {
@@ -182,9 +195,10 @@ type RpbYokozunaSchemaGetReq struct {
 	XXX_unrecognized []byte `json:"-"`
 }
 
-func (m *RpbYokozunaSchemaGetReq) Reset()         { *m = RpbYokozunaSchemaGetReq{} }
-func (m *RpbYokozunaSchemaGetReq) String() string { return proto.CompactTextString(m) }
-func (*RpbYokozunaSchemaGetReq) ProtoMessage()    {}
+func (m *RpbYokozunaSchemaGetReq) Reset()                    { *m = RpbYokozunaSchemaGetReq{} }
+func (m *RpbYokozunaSchemaGetReq) String() string            { return proto.CompactTextString(m) }
+func (*RpbYokozunaSchemaGetReq) ProtoMessage()               {}
+func (*RpbYokozunaSchemaGetReq) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{7} }
 
 func (m *RpbYokozunaSchemaGetReq) GetName() []byte {
 	if m != nil {
@@ -198,9 +212,10 @@ type RpbYokozunaSchemaGetResp struct {
 	XXX_unrecognized []byte             `json:"-"`
 }
 
-func (m *RpbYokozunaSchemaGetResp) Reset()         { *m = RpbYokozunaSchemaGetResp{} }
-func (m *RpbYokozunaSchemaGetResp) String() string { return proto.CompactTextString(m) }
-func (*RpbYokozunaSchemaGetResp) ProtoMessage()    {}
+func (m *RpbYokozunaSchemaGetResp) Reset()                    { *m = RpbYokozunaSchemaGetResp{} }
+func (m *RpbYokozunaSchemaGetResp) String() string            { return proto.CompactTextString(m) }
+func (*RpbYokozunaSchemaGetResp) ProtoMessage()               {}
+func (*RpbYokozunaSchemaGetResp) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{8} }
 
 func (m *RpbYokozunaSchemaGetResp) GetSchema() *RpbYokozunaSchema {
 	if m != nil {
@@ -210,4 +225,35 @@ func (m *RpbYokozunaSchemaGetResp) GetSchema() *RpbYokozunaSchema {
 }
 
 func init() {
+	proto.RegisterType((*RpbYokozunaIndex)(nil), "RpbYokozunaIndex")
+	proto.RegisterType((*RpbYokozunaIndexGetReq)(nil), "RpbYokozunaIndexGetReq")
+	proto.RegisterType((*RpbYokozunaIndexGetResp)(nil), "RpbYokozunaIndexGetResp")
+	proto.RegisterType((*RpbYokozunaIndexPutReq)(nil), "RpbYokozunaIndexPutReq")
+	proto.RegisterType((*RpbYokozunaIndexDeleteReq)(nil), "RpbYokozunaIndexDeleteReq")
+	proto.RegisterType((*RpbYokozunaSchema)(nil), "RpbYokozunaSchema")
+	proto.RegisterType((*RpbYokozunaSchemaPutReq)(nil), "RpbYokozunaSchemaPutReq")
+	proto.RegisterType((*RpbYokozunaSchemaGetReq)(nil), "RpbYokozunaSchemaGetReq")
+	proto.RegisterType((*RpbYokozunaSchemaGetResp)(nil), "RpbYokozunaSchemaGetResp")
+}
+
+var fileDescriptor0 = []byte{
+	// 279 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0x8c, 0x92, 0x4f, 0x4b, 0xc3, 0x40,
+	0x10, 0xc5, 0x69, 0x6a, 0x15, 0xc6, 0xa4, 0xda, 0x15, 0x6c, 0xbc, 0x85, 0x39, 0x68, 0x7b, 0xd9,
+	0x43, 0xaf, 0xa2, 0x42, 0x10, 0x44, 0xbc, 0x94, 0x78, 0xf2, 0x54, 0x36, 0x71, 0xa4, 0xa1, 0xcd,
+	0x6e, 0x6c, 0x36, 0xa2, 0x7e, 0x7a, 0x37, 0x49, 0x95, 0xfc, 0x3b, 0x78, 0x9c, 0xe5, 0xf7, 0xde,
+	0xbc, 0x79, 0x2c, 0x9c, 0xed, 0x62, 0xb1, 0x59, 0x7d, 0xa9, 0x8d, 0xfa, 0xce, 0xa5, 0xe0, 0xe9,
+	0x4e, 0x69, 0x85, 0x77, 0x70, 0x1a, 0xa4, 0xe1, 0xcb, 0xfe, 0xf1, 0x51, 0xbe, 0xd2, 0x27, 0xb3,
+	0xe1, 0x40, 0x8a, 0x84, 0xdc, 0x81, 0x67, 0xcd, 0x6c, 0x36, 0x86, 0xc3, 0x2c, 0x5a, 0x53, 0x22,
+	0x5c, 0xcb, 0x1b, 0x98, 0xd9, 0x81, 0x91, 0x5c, 0x7d, 0x88, 0xad, 0x3b, 0x34, 0xa3, 0x83, 0x97,
+	0x70, 0xde, 0x36, 0x78, 0x20, 0x1d, 0xd0, 0x7b, 0xcd, 0xc6, 0xc8, 0xf0, 0x1a, 0xa6, 0xbd, 0x5c,
+	0x96, 0x32, 0x0f, 0x46, 0x71, 0x31, 0x1b, 0x72, 0x38, 0x3b, 0x5e, 0x4c, 0x78, 0x1b, 0xc4, 0xa7,
+	0xee, 0x92, 0x65, 0x5e, 0x2e, 0xa9, 0x69, 0xad, 0x5e, 0x2d, 0x3b, 0x81, 0x23, 0x1d, 0x27, 0xa4,
+	0x72, 0x5d, 0x1e, 0xe0, 0xe0, 0x1c, 0x2e, 0xda, 0xd0, 0x3d, 0x6d, 0x49, 0x53, 0x33, 0xb4, 0xb9,
+	0x1d, 0x17, 0x30, 0xa9, 0xa1, 0xcf, 0x65, 0x0d, 0xad, 0x7a, 0x8c, 0x7d, 0xa4, 0xa4, 0x26, 0x59,
+	0xd9, 0xdb, 0x78, 0xd3, 0x38, 0xb4, 0xd2, 0xec, 0xc3, 0xe2, 0x5f, 0x95, 0x55, 0x5a, 0xc6, 0x3b,
+	0x24, 0x5e, 0xf5, 0xc8, 0x3b, 0x85, 0x16, 0xd9, 0x6e, 0xc1, 0xed, 0x07, 0x4d, 0xa3, 0xff, 0x58,
+	0xe4, 0xcf, 0x61, 0x1a, 0xa9, 0x84, 0x87, 0x22, 0x5b, 0x2b, 0x5e, 0x7c, 0x8d, 0xea, 0x47, 0x84,
+	0xf9, 0x9b, 0x3f, 0x0e, 0xcc, 0xf8, 0x8b, 0x2f, 0xfd, 0x9f, 0x00, 0x00, 0x00, 0xff, 0xff, 0xf5,
+	0x13, 0xa5, 0x19, 0x3a, 0x02, 0x00, 0x00,
 }
