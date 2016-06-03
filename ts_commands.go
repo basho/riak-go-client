fd490178b0960298da2e1d61192c13e58131306a
Christopher Mancini <cmancini@basho.com>
2016-06-03 13:00:57 -0400

    Finish up unit tests. Properly align the commands with the protobuf structs. Cleanup the docblocks. Fix bug/s with row conversions.

--- ts_commands.go
+++ ts_commands.go
@@ -34,27 +34,27 @@ func (c *TsCell) GetDataType() string {
 	return dType
 }
 
-// GetStringValue returns the data type of the value stored within the cell
+// GetStringValue returns the string value stored within the cell
 func (c *TsCell) GetStringValue() string {
 	return string(c.cell.GetVarcharValue())
 }
 
-// GetBooleanValue returns the data type of the value stored within the cell
+// GetBooleanValue returns the boolean value stored within the cell
 func (c *TsCell) GetBooleanValue() bool {
 	return c.cell.GetBooleanValue()
 }
 
-// GetDoubleValue returns the data type of the value stored within the cell
+// GetDoubleValue returns the double value stored within the cell
 func (c *TsCell) GetDoubleValue() float64 {
 	return c.cell.GetDoubleValue()
 }
 
-// GetSint64Value returns the data type of the value stored within the cell
+// GetSint64Value returns the sint64 value stored within the cell
 func (c *TsCell) GetSint64Value() int64 {
 	return c.cell.GetSint64Value()
 }
 
-// GetTimestampValue returns the data type of the value stored within the cell
+// GetTimestampValue returns the timestamp value stored within the cell
 func (c *TsCell) GetTimestampValue() int64 {
 	return c.cell.GetTimestampValue()
 }
@@ -116,10 +116,9 @@ func (c *TsColumnDescription) setColumn(tsCol *riak_ts.TsColumnDescription) {
 // TsPutReq
 // TsPutResp
 
-// TsStoreRowsCommand is sused to store a new search index on Riak
+// TsStoreRowsCommand is sused to store a new row/s in Riak TS
 type TsStoreRowsCommand struct {
 	commandImpl
-	timeoutImpl
 	retryableCommandImpl
 	Response bool
 	protobuf *riak_ts.TsPutReq
@@ -156,10 +155,9 @@ func (cmd *TsStoreRowsCommand) getResponseProtobufMessage() proto.Message {
 //
 //	command := NewTsStoreRowsCommandBuilder().
 //		WithTable("myTableName").
-//		WithRows().
+//		WithRows(rows).
 //		Build()
 type TsStoreRowsCommandBuilder struct {
-	timeout  time.Duration
 	protobuf *riak_ts.TsPutReq
 }
 
@@ -168,13 +166,13 @@ func NewTsStoreRowsCommandBuilder() *TsStoreRowsCommandBuilder {
 	return &TsStoreRowsCommandBuilder{protobuf: &riak_ts.TsPutReq{}}
 }
 
-// WithTable sets the index to use for the command
+// WithTable sets the table to use for the command
 func (builder *TsStoreRowsCommandBuilder) WithTable(table string) *TsStoreRowsCommandBuilder {
 	builder.protobuf.Table = []byte(table)
 	return builder
 }
 
-// WithRows sets the index to use for the command
+// WithRows sets the rows to be stored by the command
 func (builder *TsStoreRowsCommandBuilder) WithRows(rows [][]TsCell) *TsStoreRowsCommandBuilder {
 	builder.protobuf.Rows = convertFromTsRows(rows)
 	return builder
@@ -191,9 +189,6 @@ func (builder *TsStoreRowsCommandBuilder) Build() (Command, error) {
 	}
 
 	return &TsStoreRowsCommand{
-		timeoutImpl: timeoutImpl{
-			timeout: builder.timeout,
-		},
 		protobuf: builder.protobuf,
 	}, nil
 }
@@ -287,7 +282,7 @@ func NewTsFetchRowCommandBuilder() *TsFetchRowCommandBuilder {
 	return builder
 }
 
-// WithTable sets the bucket to be used by the command
+// WithTable sets the table to be used by the command
 func (builder *TsFetchRowCommandBuilder) WithTable(table string) *TsFetchRowCommandBuilder {
 	builder.protobuf.Table = []byte(table)
 	return builder
@@ -303,6 +298,14 @@ func (builder *TsFetchRowCommandBuilder) WithKey(key []TsCell) *TsFetchRowComman
 
 	builder.protobuf.Key = tsKey
 
+	return builder
+}
+
+// WithTimeout sets a timeout in milliseconds to be used for this command operation
+func (builder *TsFetchRowCommandBuilder) WithTimeout(timeout time.Duration) *TsFetchRowCommandBuilder {
+	timeoutMilliseconds := uint32(timeout / time.Millisecond)
+	builder.timeout = timeout
+	builder.protobuf.Timeout = &timeoutMilliseconds
 	return builder
 }
 
@@ -328,12 +331,12 @@ func (builder *TsFetchRowCommandBuilder) Build() (Command, error) {
 	}, nil
 }
 
-// TsDeleteRowCommand is used to fetch / get a value from Riak KV
+// TsDeleteRowCommand is used to delete a value from Riak TS
 type TsDeleteRowCommand struct {
 	commandImpl
 	timeoutImpl
 	retryableCommandImpl
-	Response *TsDeleteRowResponse
+	Response bool
 	protobuf *riak_ts.TsDelReq
 }
 
@@ -348,12 +351,7 @@ func (cmd *TsDeleteRowCommand) constructPbRequest() (proto.Message, error) {
 
 func (cmd *TsDeleteRowCommand) onSuccess(msg proto.Message) error {
 	cmd.success = true
-
-	if msg == nil {
-		cmd.Response = &TsDeleteRowResponse{
-			IsNotFound: true,
-		}
-	}
+	cmd.Response = true
 	return nil
 }
 
@@ -367,11 +365,6 @@ func (cmd *TsDeleteRowCommand) getResponseCode() byte {
 
 func (cmd *TsDeleteRowCommand) getResponseProtobufMessage() proto.Message {
 	return &riak_ts.TsDelResp{}
-}
-
-// TsDeleteRowResponse contains the response data for a TsDeleteRowCommand
-type TsDeleteRowResponse struct {
-	IsNotFound bool
 }
 
 // TsDeleteRowCommandBuilder type is required for creating new instances of TsDeleteRowCommand
@@ -391,7 +384,7 @@ func NewTsDeleteRowCommandBuilder() *TsDeleteRowCommandBuilder {
 	return builder
 }
 
-// WithTable sets the bucket to be used by the command
+// WithTable sets the table to be used by the command
 func (builder *TsDeleteRowCommandBuilder) WithTable(table string) *TsDeleteRowCommandBuilder {
 	builder.protobuf.Table = []byte(table)
 	return builder
@@ -407,6 +400,14 @@ func (builder *TsDeleteRowCommandBuilder) WithKey(key []TsCell) *TsDeleteRowComm
 
 	builder.protobuf.Key = tsKey
 
+	return builder
+}
+
+// WithTimeout sets a timeout in milliseconds to be used for this command operation
+func (builder *TsDeleteRowCommandBuilder) WithTimeout(timeout time.Duration) *TsDeleteRowCommandBuilder {
+	timeoutMilliseconds := uint32(timeout / time.Millisecond)
+	builder.timeout = timeout
+	builder.protobuf.Timeout = &timeoutMilliseconds
 	return builder
 }
 
@@ -432,15 +433,13 @@ func (builder *TsDeleteRowCommandBuilder) Build() (Command, error) {
 	}, nil
 }
 
-// TsQueryCommand is used to fetch / get a value from Riak KV
+// TsQueryCommand is used to fetch / get a value from Riak TS
 type TsQueryCommand struct {
 	commandImpl
-	timeoutImpl
-	Response  *TsQueryResponse
-	protobuf  *riak_ts.TsQueryReq
-	streaming bool
-	callback  func([][]TsCell) error
-	done      bool
+	Response *TsQueryResponse
+	protobuf *riak_ts.TsQueryReq
+	callback func([][]TsCell) error
+	done     bool
 }
 
 // Name identifies this command
@@ -484,7 +483,7 @@ func (cmd *TsQueryCommand) onSuccess(msg proto.Message) error {
 
 				rows := convertFromPbTsRows(tsRows, columnCount)
 
-				if cmd.streaming {
+				if cmd.protobuf.GetStream() {
 					if cmd.callback == nil {
 						panic("[TsQueryCommand] requires a callback when streaming.")
 					} else {
@@ -511,7 +510,7 @@ func (cmd *TsQueryCommand) getRequestCode() byte {
 }
 
 func (cmd *TsQueryCommand) getResponseCode() byte {
-	return rpbCode_TsDelResp
+	return rpbCode_TsQueryResp
 }
 
 func (cmd *TsQueryCommand) getResponseProtobufMessage() proto.Message {
@@ -532,10 +531,8 @@ type TsQueryResponse struct {
 //		WithCallback(cb).
 //		Build()
 type TsQueryCommandBuilder struct {
-	timeout   time.Duration
-	protobuf  *riak_ts.TsQueryReq
-	streaming bool
-	callback  func(rows [][]TsCell) error
+	protobuf *riak_ts.TsQueryReq
+	callback func(rows [][]TsCell) error
 }
 
 // NewTsQueryCommandBuilder is a factory function for generating the command builder struct
@@ -544,9 +541,9 @@ func NewTsQueryCommandBuilder() *TsQueryCommandBuilder {
 	return builder
 }
 
-// WithTsQuery sets the bucket to be used by the command
-func (builder *TsQueryCommandBuilder) WithTsQuery(Tsquery string) *TsQueryCommandBuilder {
-	builder.protobuf.Query = &riak_ts.TsInterpolation{Base: []byte(Tsquery)}
+// WithQuery sets the query to be used by the command
+func (builder *TsQueryCommandBuilder) WithQuery(query string) *TsQueryCommandBuilder {
+	builder.protobuf.Query = &riak_ts.TsInterpolation{Base: []byte(query)}
 	return builder
 }
 
@@ -554,7 +551,7 @@ func (builder *TsQueryCommandBuilder) WithTsQuery(Tsquery string) *TsQueryComman
 //
 // If true, a callback must be provided via WithCallback()
 func (builder *TsQueryCommandBuilder) WithStreaming(streaming bool) *TsQueryCommandBuilder {
-	builder.streaming = streaming
+	builder.protobuf.Stream = &streaming
 	return builder
 }
 
@@ -576,11 +573,13 @@ func (builder *TsQueryCommandBuilder) Build() (Command, error) {
 		return nil, ErrQueryRequired
 	}
 
+	if builder.protobuf.GetStream() && builder.callback == nil {
+		return nil, newClientError("TsQueryCommand requires a callback when streaming.", nil)
+	}
+
 	return &TsQueryCommand{
-		timeoutImpl: timeoutImpl{
-			timeout: builder.timeout,
-		},
 		protobuf: builder.protobuf,
+		callback: builder.callback,
 	}, nil
 }
 
@@ -588,7 +587,7 @@ func (builder *TsQueryCommandBuilder) Build() (Command, error) {
 // TsListKeysReq
 // TsListKeysResp
 
-// TsListKeysCommand is used to fetch / get a value from Riak KV
+// TsListKeysCommand is used to fetch values from a table in Riak TS
 type TsListKeysCommand struct {
 	commandImpl
 	timeoutImpl
@@ -696,7 +695,7 @@ func NewTsListKeysCommandBuilder() *TsListKeysCommandBuilder {
 	return builder
 }
 
-// WithTable sets the bucket to be used by the command
+// WithTable sets the table to be used by the command
 func (builder *TsListKeysCommandBuilder) WithTable(table string) *TsListKeysCommandBuilder {
 	builder.protobuf.Table = []byte(table)
 	return builder
@@ -715,6 +714,14 @@ func (builder *TsListKeysCommandBuilder) WithStreaming(streaming bool) *TsListKe
 // Requires WithStreaming(true)
 func (builder *TsListKeysCommandBuilder) WithCallback(callback func([]string) error) *TsListKeysCommandBuilder {
 	builder.callback = callback
+	return builder
+}
+
+// WithTimeout sets a timeout in milliseconds to be used for this command operation
+func (builder *TsListKeysCommandBuilder) WithTimeout(timeout time.Duration) *TsListKeysCommandBuilder {
+	timeoutMilliseconds := uint32(timeout / time.Millisecond)
+	builder.timeout = timeout
+	builder.protobuf.Timeout = &timeoutMilliseconds
 	return builder
 }
 
@@ -742,9 +749,11 @@ func (builder *TsListKeysCommandBuilder) Build() (Command, error) {
 	}, nil
 }
 
+// Converts a slice of riak_ts.TsRow to a slice of .TsRows
 func convertFromPbTsRows(tsRows []*riak_ts.TsRow, columnCount int) [][]TsCell {
 	var rows [][]TsCell
 	var cell TsCell
+	rowCount := len(tsRows)
 
 	for i, tsRow := range tsRows {
 		r := make([]TsCell, columnCount)
@@ -755,7 +764,7 @@ func convertFromPbTsRows(tsRows []*riak_ts.TsRow, columnCount int) [][]TsCell {
 		}
 
 		if len(rows) < 1 {
-			rows = make([][]TsCell, 0)
+			rows = make([][]TsCell, rowCount)
 		}
 
 		rows[i] = r
@@ -764,18 +773,21 @@ func convertFromPbTsRows(tsRows []*riak_ts.TsRow, columnCount int) [][]TsCell {
 	return rows
 }
 
+// Converts a slice of .TsRows to a slice of riak_ts.TsRow
 func convertFromTsRows(tsRows [][]TsCell) []*riak_ts.TsRow {
 	var rows []*riak_ts.TsRow
+	rowCount := len(tsRows)
+	cellCount := len(tsRows[0])
 
 	for i, tsRow := range tsRows {
-		cells := make([]*riak_ts.TsCell, 0)
+		cells := make([]*riak_ts.TsCell, cellCount)
 
 		for k, tsCell := range tsRow {
 			cells[k] = tsCell.cell
 		}
 
 		if len(rows) < 1 {
-			rows = make([]*riak_ts.TsRow, 0)
+			rows = make([]*riak_ts.TsRow, rowCount)
 		}
 
 		rows[i] = &riak_ts.TsRow{Cells: cells}
--- ts_commands_test.go
+++ ts_commands_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/basho/riak-go-client/rpb/riak_ts"
-	"github.com/golang/protobuf/proto"
 )
 
 func TestBuildTsGetReqCorrectlyViaBuilder(t *testing.T) {
@@ -35,11 +34,63 @@ func TestBuildTsGetReqCorrectlyViaBuilder(t *testing.T) {
 	if protobuf == nil {
 		t.FailNow()
 	}
-	validateTsGetReq(t, protobuf)
+
+	if req, ok := protobuf.(*riak_ts.TsGetReq); ok {
+		if expected, actual := "table_name", string(req.GetTable()); expected != actual {
+			t.Errorf("expected %v, got %v", expected, actual)
+		}
+
+		if expected, actual := 3, len(req.GetKey()); expected != actual {
+			t.Errorf("expected %v, got %v", expected, actual)
+		}
+	} else {
+		t.Errorf("ok: %v - could not convert %v to *riak_ts.TsGetReq", ok, reflect.TypeOf(protobuf))
+	}
+}
+
+func TestBuildTsDelReqCorrectlyViaBuilder(t *testing.T) {
+	key := make([]TsCell, 3)
+
+	key[0] = NewStringTsCell("Test Key Value")
+	key[1] = NewSint64TsCell(1)
+	key[2] = NewDoubleTsCell(0.1)
+
+	builder := NewTsFetchRowCommandBuilder().
+		WithTable("table_name").
+		WithKey(key)
+
+	cmd, err := builder.Build()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if _, ok := cmd.(retryableCommand); !ok {
+		t.Errorf("got %v, want cmd %s to implement retryableCommand", ok, reflect.TypeOf(cmd))
+	}
+
+	protobuf, err := cmd.constructPbRequest()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if protobuf == nil {
+		t.FailNow()
+	}
+
+	if req, ok := protobuf.(*riak_ts.TsDelReq); ok {
+		if expected, actual := "table_name", string(req.GetTable()); expected != actual {
+			t.Errorf("expected %v, got %v", expected, actual)
+		}
+
+		if expected, actual := 3, len(req.GetKey()); expected != actual {
+			t.Errorf("expected %v, got %v", expected, actual)
+		}
+	} else {
+		t.Errorf("ok: %v - could not convert %v to *riak_ts.TsDelReq", ok, reflect.TypeOf(protobuf))
+	}
 }
 
 func TestBuildTsPutReqCorrectlyViaBuilder(t *testing.T) {
-	row := make([]TsCell, 3)
+	row := make([]TsCell, 5)
 
 	row[0] = NewStringTsCell("Test Key Value")
 	row[1] = NewSint64TsCell(1)
@@ -47,7 +98,7 @@ func TestBuildTsPutReqCorrectlyViaBuilder(t *testing.T) {
 	row[3] = NewBooleanTsCell(true)
 	row[4] = NewTimestampTsCell(1234567890)
 
-	rows := make([][]TsCell, 0)
+	rows := make([][]TsCell, 1)
 	rows[0] = row
 
 	builder := NewTsStoreRowsCommandBuilder().
@@ -81,20 +132,115 @@ func TestBuildTsPutReqCorrectlyViaBuilder(t *testing.T) {
 			t.Errorf("expected %v, got %v", expected, actual)
 		}
 	} else {
-		t.Errorf("ok: %v - could not convert %v to *rpbRiakKV.RpbGetReq", ok, reflect.TypeOf(protobuf))
+		t.Errorf("ok: %v - could not convert %v to *riak_ts.TsPutReq", ok, reflect.TypeOf(protobuf))
 	}
 }
 
-func validateTsGetReq(t *testing.T, protobuf proto.Message) {
-	if req, ok := protobuf.(*riak_ts.TsGetReq); ok {
-		if expected, actual := "table_name", string(req.GetTable()); expected != actual {
+func TestBuildTsQueryReqCorrectlyViaBuilder(t *testing.T) {
+
+	builder := NewTsQueryCommandBuilder().
+		WithQuery("DESCRIBE table_name")
+
+	if builder.protobuf.GetStream() != false {
+		t.Errorf("expected %v, got %v", nil, builder.protobuf.GetStream())
+	}
+
+	cmd, err := builder.Build()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	builder.WithStreaming(true)
+	if expected, actual := true, builder.protobuf.GetStream(); expected != actual {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+
+	cmd, err = builder.Build()
+	if err == nil {
+		t.Fatal("Expected an error, you cannot build the command with streaming true and callback = nil")
+	}
+
+	cb := func(rows [][]TsCell) error {
+		// do stuff
+		return nil
+	}
+
+	builder.WithCallback(cb)
+
+	cmd, err = builder.Build()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	protobuf, err := cmd.constructPbRequest()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if protobuf == nil {
+		t.FailNow()
+	}
+
+	if req, ok := protobuf.(*riak_ts.TsQueryReq); ok {
+		if expected, actual := "DESCRIBE table_name", string(req.GetQuery().GetBase()); expected != actual {
 			t.Errorf("expected %v, got %v", expected, actual)
 		}
-
-		if expected, actual := 3, len(req.GetKey()); expected != actual {
+		if expected, actual := true, req.GetStream(); expected != actual {
 			t.Errorf("expected %v, got %v", expected, actual)
 		}
 	} else {
-		t.Errorf("ok: %v - could not convert %v to *rpbRiakKV.RpbGetReq", ok, reflect.TypeOf(protobuf))
+		t.Errorf("ok: %v - could not convert %v to *riak_ts.TsQueryReq", ok, reflect.TypeOf(protobuf))
+	}
+}
+
+func TestBuildTsListKeysReqCorrectlyViaBuilder(t *testing.T) {
+	builder := NewTsListKeysCommandBuilder().
+		WithTable("table_name")
+
+	if expected, actual := false, builder.streaming; expected != actual {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+
+	cmd, err := builder.Build()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	builder.WithStreaming(true)
+	if expected, actual := true, builder.streaming; expected != actual {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+
+	cmd, err = builder.Build()
+	if err == nil {
+		t.Fatal("Expected an error, you cannot build the command with streaming true and callback = nil")
+	}
+
+	cb := func(keys []string) error {
+		// do stuff
+		return nil
+	}
+
+	builder.WithCallback(cb)
+	cmd, err = builder.Build()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	protobuf, err := cmd.constructPbRequest()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if protobuf == nil {
+		t.FailNow()
+	}
+
+	if req, ok := protobuf.(*riak_ts.TsListKeysReq); ok {
+		if expected, actual := "table_name", string(req.GetTable()); expected != actual {
+			t.Errorf("expected %v, got %v", expected, actual)
+		}
+	} else {
+		t.Errorf("ok: %v - could not convert %v to *riak_ts.TsListKeysReq", ok, reflect.TypeOf(protobuf))
 	}
 }
