04e1346542390a18990212f89942ed2d35959dbc
Luke Bakken <lbakken@basho.com>
2016-06-14 13:08:35 -0700

    Added make target to run timeseries tests

--- build/common.mk
+++ build/common.mk
@@ -16,6 +16,9 @@ unit-test: lint
 integration-test: lint
 	go test -v -tags=integration github.com/basho/riak-go-client/...
 
+timeseries-test:
+	go test -v -tags=timeseries github.com/basho/riak-go-client/...
+
 test: integration-test
 
 fmt:
@@ -27,12 +30,13 @@ protogen:
 help:
 	@echo ''
 	@echo ' Targets:'
-	@echo '--------------------------------------------------'
-	@echo ' all              - Run everything                '
-	@echo ' fmt              - Format code                   '
-	@echo ' lint             - Run "go vet"                  '
-	@echo ' test             - Run unit & integration tests  '
-	@echo ' unit-test        - Run unit tests                '
-	@echo ' integration-test - Run integration tests         '
-	@echo '--------------------------------------------------'
+	@echo '-----------------------------------------------------'
+	@echo ' all              - Run everything                   '
+	@echo ' fmt              - Format code                      '
+	@echo ' lint             - Run "go vet"                     '
+	@echo ' test             - Run unit & integration tests     '
+	@echo ' unit-test        - Run unit tests                   '
+	@echo ' integration-test - Run integration tests            '
+	@echo ' timeseries-test  - Run timeseries integration tests '
+	@echo '-----------------------------------------------------'
 	@echo ''
