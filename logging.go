0f4b91816a6c6ffb0e3bbf771c6fe9e787061b70
Michael Porter <the.germanboy@gmail.com>
2017-08-03 22:03:10 -0400

    Add and use Logger / FancyLogger interface to permit using loggers other than stdlib, eg. golog, logrus, etc.

--- logging.go
+++ logging.go
@@ -22,64 +22,113 @@ import (
 	"os"
 )
 
+// Logger is a generic logging interface, to allow utilizing
+// any stdlib-compatible logger for logging
+type Logger interface {
+	Printf(format string, v ...interface{})
+	Println(v ...interface{})
+}
+
+// FancyLogger represents a leveled logger, with methods representing
+// debug, warn, and error levels
+type FancyLogger interface {
+	Logger
+	Debug(v ...interface{})
+	Debugf(format string, v ...interface{})
+	Warn(v ...interface{})
+	Warnf(format string, v ...interface{})
+	Error(v ...interface{})
+	Errorf(format string, v ...interface{})
+}
+
 // If true, debug messages will be written to the log
 var EnableDebugLogging = false
 
-var errLogger = log.New(os.Stderr, "", log.LstdFlags)
-var stdLogger = log.New(os.Stderr, "", log.LstdFlags)
+var errLogger Logger
+var stdLogger Logger
 
 func init() {
 	if debugEnvVar := os.Getenv("RIAK_GO_CLIENT_DEBUG"); debugEnvVar != "" {
 		EnableDebugLogging = true
 	}
+	errLogger = log.New(os.Stderr, "", log.LstdFlags)
+	stdLogger = log.New(os.Stderr, "", log.LstdFlags)
 }
 
 // SetLogger sets the standard logger used for
 // WARN and DEBUG (if enabled)
-func SetLogger(logger *log.Logger) {
+func SetLogger(logger Logger) {
 	stdLogger = logger
 }
 
 // SetErrorLogger sets the logger used for errors
-func SetErrorLogger(logger *log.Logger) {
+func SetErrorLogger(logger Logger) {
 	errLogger = logger
 }
 
 // logDebug writes formatted string debug messages using Printf only if debug logging is enabled
 func logDebug(source, format string, v ...interface{}) {
 	if EnableDebugLogging {
-		stdLogger.Printf(fmt.Sprintf("[DEBUG] %s %s", source, format), v...)
+		if fancyLogger, ok := stdLogger.(FancyLogger); ok {
+			fancyLogger.Debugf(fmt.Sprintf("%s %s", source, format), v...)
+		} else {
+			stdLogger.Printf(fmt.Sprintf("[DEBUG] %s %s", source, format), v...)
+		}
 	}
 }
 
 // logDebugln writes string debug messages using Println
 func logDebugln(source string, v ...interface{}) {
 	if EnableDebugLogging {
-		stdLogger.Println("[DEBUG]", source, v)
+		if fancyLogger, ok := stdLogger.(FancyLogger); ok {
+			fancyLogger.Debug(source, v)
+		} else {
+			stdLogger.Println("[DEBUG]", source, v)
+		}
 	}
 }
 
 // logWarn writes formatted string warning messages using Printf
 func logWarn(source, format string, v ...interface{}) {
-	stdLogger.Printf(fmt.Sprintf("[WARNING] %s %s", source, format), v...)
+	if fancyLogger, ok := stdLogger.(FancyLogger); ok {
+		fancyLogger.Warnf(fmt.Sprintf("%s %s", source, format), v...)
+	} else {
+		stdLogger.Printf(fmt.Sprintf("[WARNING] %s %s", source, format), v...)
+	}
 }
 
 // logWarnln writes string warning messages using Println
 func logWarnln(source string, v ...interface{}) {
-	stdLogger.Println("[WARNING]", source, v)
+	if fancyLogger, ok := stdLogger.(FancyLogger); ok {
+		fancyLogger.Warn(source, v)
+	} else {
+		stdLogger.Println("[WARNING]", source, v)
+	}
 }
 
 // logError writes formatted string error messages using Printf
 func logError(source, format string, v ...interface{}) {
-	errLogger.Printf(fmt.Sprintf("[ERROR] %s %s", source, format), v...)
+	if fancyLogger, ok := errLogger.(FancyLogger); ok {
+		fancyLogger.Errorf(fmt.Sprintf("%s %s", source, format), v...)
+	} else {
+		errLogger.Printf(fmt.Sprintf("[ERROR] %s %s", source, format), v...)
+	}
 }
 
 // logErr writes err.Error() using Println
 func logErr(source string, err error) {
-	errLogger.Println("[ERROR]", source, err)
+	if fancyLogger, ok := errLogger.(FancyLogger); ok {
+		fancyLogger.Error(source, err)
+	} else {
+		errLogger.Println("[ERROR]", source, err)
+	}
 }
 
 // logErrorln writes an error message using Println
 func logErrorln(source string, v ...interface{}) {
-	errLogger.Println("[ERROR]", source, v)
+	if fancyLogger, ok := errLogger.(FancyLogger); ok {
+		fancyLogger.Error(source, v)
+	} else {
+		errLogger.Println("[ERROR]", source, v)
+	}
 }
